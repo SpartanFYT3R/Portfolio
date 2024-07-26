48be6cb6c4fbc704ed9fa9661767f247ff6debef
FYT3RP4TIL <sudhanshu21032002@gmail.com>
2024-07-27 02:55:43 +0530

    First Ed

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  MatrixRainingCode,
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <MatrixRainingCode />
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
